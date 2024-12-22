1c8a37283b92189891bc1d4f5c2a5cf6738beeaa
Adonis_Dulaurent <adonis.dulaurent@gmail.com>
2024-12-22 18:36:56 +0100

    ad comment

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -3,16 +3,29 @@ from ..app import app
 from flask import render_template
 import requests
 
-
+#road when you arrive on the websibe 
 @app.route("/")
 @app.route("/home")
 def home ():
     return "Hello world but enter retrive_wikidata + id"
 
-# Route get avec paramètres : 
+# Get road with parameters : 
 @app.route("/retrive_wikidata/<id>")
 def retrive_wikidata(id):
-    """ 
+    """Function that uses Wikidata API to retrieve entity data in JSON format.
+
+    Args:
+        id (str): The ID of the wikidata entity to retrieve
+        ex. Q42 = Douglas Adams
+
+    Returns:
+        Flask.reponse : A rendered HTML template (display_wikidata.html) with the following context variables:
+        - id (str): the Wikidata entity ID.
+        - status_codes (int): The htpp status code of the API reponse:
+            - 200 if the request is succesful
+            - 404 if the servers does not reponse.
+        - content (str): The content type of the API reponse 
+        - json (dict. or None) : The JSON data retrieved from wikidata, or None if the request failed. 
     """
     base_url ="https://www.wikidata.org/wiki/Special:EntityData"
     json = None
